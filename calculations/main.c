7c58179d279941942e5183aadaec547ae5c26bb5
Eatedal makram <88502446+amiramakram@users.noreply.github.com>
2022-11-28 01:06:08 +0200

    Add files via upload

---
+++ calculations/main.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    //sum
+    int x=3;
+    int y=4;
+    int sum=x+y;
+
+    printf("value of sum= %d\n",sum);
+    //subtraction
+    int sub=y-x;
+
+    printf("value of sub= %d\n",sub);
+    //division
+
+    int s=2;
+    int div=y/s;
+    printf("value of div= %d\n",div);
+    //multiplication
+    int mult=x*y;
+    printf("value of mult= %d",mult);
+
+    return 0;
+}
